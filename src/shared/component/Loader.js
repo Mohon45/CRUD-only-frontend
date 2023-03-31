57c9f4736f8894cc6a3c00e984c0216f21fa8292
Md: Mohon <mdmohon6145@gmail.com>
2023-03-31 17:40:28 +0600

    loader backgeound change

--- src/shared/component/Loader.js
+++ src/shared/component/Loader.js
@@ -3,8 +3,14 @@ import { HashLoader } from "react-spinners";
 import "./Loader.css";
 
 export default function Loader({ active, children }) {
+  const COLORS = {
+    PRIMARY: "#2FA84F",
+  };
   return (
-    <LoadingOverlay active={active} spinner={<HashLoader />}>
+    <LoadingOverlay
+      active={active}
+      spinner={<HashLoader color={COLORS.PRIMARY} />}
+    >
       {children}
     </LoadingOverlay>
   );
